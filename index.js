d1537dac817ee49fe75402af62246a1d70f1e5e6
Ellis Goldbas <ellisgoldbas@seattleacademy.org>
2021-05-03 10:54:09 -0700

    Add express.js

---
+++ index.js
@@ -0,0 +1,28 @@
+{
+  "name": "2134-3",
+  "version": "0.0.2",
+  "description": "learn express js",
+  "main": "index.js",
+  "scripts": {
+    "test": "robots"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/ellisgoldbas5/2134-3.git"
+  },
+  "keywords": [
+    "robots",
+    "express"
+  ],
+  "author": "Ellis Goldbas",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/ellisgoldbas5/2134-3/issues"
+  },
+  "homepage": "https://github.com/ellisgoldbas5/2134-3#readme",
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+  
+}
+
